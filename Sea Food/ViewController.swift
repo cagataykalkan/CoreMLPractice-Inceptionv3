608c6fc4bbcc75935b921b6c25d06191c66b5c23
cagataykalkan <c.kalkan203@gmail.com>
2024-12-16 14:18:06 +0300

    Initial Commit

---
+++ Sea Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sea Food
+//
+//  Created by Çağatay KALKAN on 16.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
