b4085dd89ca028b1dec588749ff002a5406e3c25
cagataykalkan <c.kalkan203@gmail.com>
2024-12-16 20:43:30 +0300

    dog

--- Sea Food/ViewController.swift
+++ Sea Food/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import Vision
+import CoreML
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("cant convert to ciimage")
+            }
+            detect(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("loading coreML failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("model failed to process image")
+            }
+            print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("rose") {
+                    self.navigationItem.title = "Dog!"
+                } else {
+                    self.navigationItem.title = "Not a dog!"
+                }
+            }
+        }
+        
+        // CIImage -> CGImage dönüşümü için CIContext kullanılıyor
+        let ciContext = CIContext()
+        guard let cgImage = ciContext.createCGImage(image, from: image.extent) else {
+            fatalError("unable to convert CIImage to CGImage")
+        }
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
